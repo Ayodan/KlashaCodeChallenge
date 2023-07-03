bb0ef56bc001ac1d686878e519874ceb9b03633a
Ayorinde Daniel <adaniel@dlocal.com>
2023-07-03 17:07:40 +0100

    added custom exception and controller advice

---
+++ src/main/java/com/klasha/code/challenge/exception/InternalServerException.java
@@ -0,0 +1,8 @@
+package com.klasha.code.challenge.exception;
+
+public class InternalServerException extends AbstractException {
+
+    public InternalServerException(String code, String message) {
+        super(code, message);
+    }
+}
